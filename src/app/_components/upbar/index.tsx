7f8dc71eea032f4096ce8104abb46d1ed6173588
Maximo <MaximoCorsani@gmail.com>
2024-06-28 11:02:59 -0300

    Fixed things

--- src/app/_components/upbar/index.tsx
+++ src/app/_components/upbar/index.tsx
@@ -5,9 +5,9 @@ export default function Upbar() {
     const { organization } = useOrganization();
 
     return (
-        <div className="w-screen h-16 shadow-md flex justify-between items-center px-4">
+        <div className="w-screen h-16 shadow-md flex justify-between items-center px-4 font-serif">
             <div className="text-lg">
-                <h1>Bienvenido {organization?.name}!</h1>
+                <h1>Bienvenido equipo de soporte {organization?.name}!</h1>
             </div>
             <div className="flex items-center p-5">
                 <SignedOut>
