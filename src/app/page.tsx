6be843e0a7fcd048cd0a8b3aa873f581cf04d79f
marcfbob <msbobzin@gmail.com>
2024-06-30 01:34:45 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useOrganization } from "@clerk/nextjs";
-
+import Notificaciones from "./_components/notificaciones/page";
+import { Title } from "./_components/ui/title";
 
 
 export default function Page() {
@@ -8,15 +9,21 @@ const organization = useOrganization()
 
 if(organization.organization?.name === "IanTech"){
 return (
-<div>
-<h1>Hola</h1>
-</div>)
+<div className="flex-auto">
+            <div className="flex flex-col align-center">
+                <Title>Notificaciones</Title>
+                <Notificaciones />
+            </div>
+        </div>)
 }  
 else{
-    return( 
-        <div>
-            <h1>Hola</h1>
-</div>
+    return (
+        <div className="flex-auto">
+            <div className="flex flex-col align-center">
+                <Title>Notificaciones</Title>
+                <Notificaciones />
+            </div>
+        </div>
     )
 }
 
