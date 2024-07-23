8dedc881a95a562341ce055453edba7e37d20a45
Maximo <MaximoCorsani@gmail.com>
2024-07-23 11:56:42 -0300

    fixed

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,4 +1,6 @@
 import Dashboard from "../_components/dashboard/dashboard";
+import Tickets from "../_components/tickets/page";
+import { Title } from "../_components/ui/title";
 
 
 export default function Page(){
@@ -7,6 +9,13 @@ export default function Page(){
             <div className='flex row-span-1 bg-slate-400 place-content-right flex-column pt-20'>
                 <Dashboard/>
             </div>
+
+            <div className="flex-auto">
+            <div className="flex flex-col align-center">
+                <Title>Todos los tickets</Title>
+                <Tickets />
+            </div>
+        </div>
         </div>
         )
 }
